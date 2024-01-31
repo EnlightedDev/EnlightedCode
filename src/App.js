8cf825e9a4761c35a01cb6274c0c5ba18c3bf525
EnlightedDev <sathwikgg98@gmail.com>
2024-01-31 18:06:52 -0500

    created react app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import MainPage from "./pages/MainPage";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+
+                <BrowserRouter>
+                <Routes>
+                    
+                    <Route path = '/' element = {<MainPage></MainPage>}></Route>
+
+                </Routes>
+                </BrowserRouter>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Editors from '../components/Editors';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Code Editor</h1>
+      <Editors></Editors>
+    </div>
+  );
+};
+
+export default MainPage;
